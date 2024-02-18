acd3a57803699f3da123f61e910c2f1bc9e35cb6
INFKIRITO <94058048+INFKIRITO@users.noreply.github.com>
2024-02-18 17:34:31 +0530

    changes

--- app.js
+++ app.js
@@ -1,29 +1,22 @@
 const express = require('express');
-
 const path = require('path');
-
-const db = require('./database/database')
-
+const db = require('./database/database');
 const authRoutes = require('./routes/auth.routes');
-
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static('public'));
-
-app.use(authRoutes); 
-
+app.use(authRoutes);
 
 db.connectToDatabase()
-.then(function(){
-  app.listen(3000)
-})
-.catch(function(error){
-  console.log('Failed to connect to the database!');
-  console.log(error);
-});
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  .then(function () {
+    app.listen(3000, () => {
+      console.log('Server is running on port 3000');
+    });
+  })
+  .catch(function (error) {
+    console.log('Failed to connect to the database!');
+    console.log(error);
   });
